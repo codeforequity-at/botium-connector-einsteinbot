04d33def5d6c27b68c802541db93c3146201a16f
Florian Treml <florian.treml@botium.at>
2020-09-07 15:56:36 +0200

    First version

---
+++ README.md
@@ -0,0 +1,106 @@
+# Botium Connector for Einstein Bots
+
+[![NPM](https://nodei.co/npm/botium-connector-einsteinbot.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/botium-connector-einsteinbot/)
+
+[![Codeship Status for codeforequity-at/botium-connector-einsteinbot](https://app.codeship.com/projects/22e46100-911b-0137-ae8a-4e8cea91d933/status?branch=master)](https://app.codeship.com/projects/356365)
+[![npm version](https://badge.fury.io/js/botium-connector-einsteinbot.svg)](https://badge.fury.io/js/botium-connector-einsteinbot)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()
+
+This is a [Botium](https://github.com/codeforequity-at/botium-core) connector for testing your Salesforce Einstein chatbot.
+
+__Did you read the [Botium in a Nutshell](https://medium.com/@floriantreml/botium-in-a-nutshell-part-1-overview-f8d0ceaf8fb4) articles? Be warned, without prior knowledge of Botium you won't be able to properly use this library!__
+
+## How it works
+Botium connects to your Einstein chatbot using the [Salesforce Live Agent REST API](https://developer.salesforce.com/docs/atlas.en-us.live_agent_rest.meta/live_agent_rest/live_agent_rest_understanding_resources.htm). 
+
+It can be used as any other Botium connector with all Botium Stack components:
+* [Botium CLI](https://github.com/codeforequity-at/botium-cli/)
+* [Botium Bindings](https://github.com/codeforequity-at/botium-bindings/)
+* [Botium Box](https://www.botium.at)
+
+## Requirements
+* **Node.js and NPM**
+* a **Einstein bot**
+* a **project directory** on your workstation to hold test cases and Botium configuration
+
+## Install Botium and Einstein Connector
+
+When using __Botium CLI__:
+
+```
+> npm install -g botium-cli
+> npm install -g botium-connector-einsteinbot
+> botium-cli init
+> botium-cli run
+```
+
+When using __Botium Bindings__:
+
+```
+> npm install -g botium-bindings
+> npm install -g botium-connector-einsteinbot
+> botium-bindings init mocha
+> npm install && npm run mocha
+```
+
+When using __Botium Box__:
+
+_Already integrated into Botium Box, no setup required_
+
+## Connecting Einstein chatbot to Botium
+
+You have to collect some information from your Salesforce instance - see capabilities description below.
+
+Create a botium.json in your project directory, filling in the required information:
+
+```
+{
+  "botium": {
+    "Capabilities": {
+      "PROJECTNAME": "<whatever>",
+      "CONTAINERMODE": "einsteinbot",
+      "EINSTEINBOT_ENDPOINT": "xxx",
+      "EINSTEINBOT_ORGANIZATION_ID": "xxx",
+      "EINSTEINBOT_DEPLOYMENT_ID": "xxx",
+      "EINSTEINBOT_CHATBUTTON_ID": "xxx"
+    }
+  }
+}
+```
+
+To check the configuration, run the emulator (Botium CLI required) to bring up a chat interface in your terminal window:
+
+```
+> botium-cli emulator
+```
+
+Botium setup is ready, you can begin to write your [BotiumScript](https://github.com/codeforequity-at/botium-core/wiki/Botium-Scripting) files.
+
+## Supported Capabilities
+
+Set the capability __CONTAINERMODE__ to __einsteinbot__ to activate this connector.
+
+### EINSTEINBOT_ENDPOINT
+Chat API Endpoint
+
+Find it in Salesforce: Service-Setup | Channels | Chat | Chat-Settings
+
+### EINSTEINBOT_ORGANIZATION_ID
+Organization Id
+
+Find it in Salesforce: Setup | Company Settings | Company information
+
+### EINSTEINBOT_DEPLOYMENT_ID
+Deployment Id
+
+Find it in Salesforce: Service-Setup | Channels | Chat | Deployments, click on deployment and get it from the page url (address=%2F<xxxxxx>)
+
+### EINSTEINBOT_CHATBUTTON_ID
+Chatbot Button Id
+
+Find it in Salesforce: Service-Setup | Channels | Chat | Chat Buttons, click on button and get it from the page url (address=%2F<xxxxxx>)
+
+### EINSTEINBOT_VISITORNAME
+Visitor name (optional)
+
+Visitor name to be used for the Botium conversations 
---
+++ index.js
@@ -0,0 +1,202 @@
+const util = require('util')
+const fs = require('fs')
+const path = require('path')
+const request = require('request-promise-native')
+const debug = require('debug')('botium-connector-einsteinbot')
+
+const SimpleRestContainer = require('botium-core/src/containers/plugins/SimpleRestContainer.js')
+const CoreCapabilities = require('botium-core/src/Capabilities')
+
+const Capabilities = {
+  EINSTEINBOT_ENDPOINT: 'EINSTEINBOT_ENDPOINT',
+  EINSTEINBOT_ORGANIZATION_ID: 'EINSTEINBOT_ORGANIZATION_ID',
+  EINSTEINBOT_DEPLOYMENT_ID: 'EINSTEINBOT_DEPLOYMENT_ID',
+  EINSTEINBOT_CHATBUTTON_ID: 'EINSTEINBOT_CHATBUTTON_ID',
+  EINSTEINBOT_VISITORNAME: 'EINSTEINBOT_VISITORNAME'
+}
+
+const Defaults = {
+  [Capabilities.EINSTEINBOT_VISITORNAME]: 'Botium Test'
+}
+
+class BotiumConnectorEinsteinBot {
+  constructor ({ queueBotSays, caps }) {
+    this.queueBotSays = queueBotSays
+    this.caps = Object.assign(caps, Defaults, caps)
+    this.delegateContainer = null
+    this.delegateCaps = null
+    this.seqCounter = null
+  }
+
+  Validate () {
+    debug('Validate called')
+    this.caps = Object.assign({}, Defaults, this.caps)
+
+    if (!this.caps[Capabilities.EINSTEINBOT_ENDPOINT]) throw new Error('EINSTEINBOT_ENDPOINT capability required')
+    if (!this.caps[Capabilities.EINSTEINBOT_ORGANIZATION_ID]) throw new Error('EINSTEINBOT_ORGANIZATION_ID capability required')
+    if (!this.caps[Capabilities.EINSTEINBOT_DEPLOYMENT_ID]) throw new Error('EINSTEINBOT_DEPLOYMENT_ID capability required')
+    if (!this.caps[Capabilities.EINSTEINBOT_CHATBUTTON_ID]) throw new Error('EINSTEINBOT_CHATBUTTON_ID capability required')
+
+    const baseUrl = `${this.caps[Capabilities.EINSTEINBOT_ENDPOINT]}${this.caps[Capabilities.EINSTEINBOT_ENDPOINT].endsWith('/') ? '' : '/'}`
+
+    if (!this.delegateContainer) {
+      // default values
+      this.delegateCaps = {
+        [CoreCapabilities.SIMPLEREST_START_HOOK]: async ({ context }) => {
+          const sessionIdRequest = {
+            uri: `${baseUrl}System/SessionId`,
+            method: 'GET',
+            headers: {
+              'X-LIVEAGENT-API-VERSION': 34,
+              'X-LIVEAGENT-AFFINITY': 'null'
+            },
+            json: true
+          }
+          debug(`SessionId Request: ${JSON.stringify(sessionIdRequest, null, 2)}`)
+          try {
+            const sessionIdResponse = await request(sessionIdRequest)
+            debug(`Got SessionId Response: ${JSON.stringify(sessionIdResponse, null, 2)}`)
+            Object.assign(context, sessionIdResponse)
+          } catch (err) {
+            throw new Error(`Failed to get Session from Einstein Endpoint: ${err.message}`)
+          }
+        },
+        [CoreCapabilities.SIMPLEREST_PING_URL]: `${baseUrl}Chasitor/ChasitorInit`,
+        [CoreCapabilities.SIMPLEREST_PING_VERB]: 'POST',
+        [CoreCapabilities.SIMPLEREST_PING_TIMEOUT]: 60000,
+        [CoreCapabilities.SIMPLEREST_PING_HEADERS]: {
+          'X-LIVEAGENT-API-VERSION': 34,
+          'X-LIVEAGENT-AFFINITY': '{{context.affinityToken}}',
+          'X-LIVEAGENT-SESSION-KEY': '{{context.key}}',
+          'X-LIVEAGENT-SEQUENCE': 1
+        },
+        [CoreCapabilities.SIMPLEREST_PING_BODY]: {
+          organizationId: this.caps[Capabilities.EINSTEINBOT_ORGANIZATION_ID],
+          deploymentId: this.caps[Capabilities.EINSTEINBOT_DEPLOYMENT_ID], 
+          buttonId: this.caps[Capabilities.EINSTEINBOT_CHATBUTTON_ID],
+          sessionId: '{{context.id}}',
+          userAgent: 'Botium',
+          language: 'en-US',
+          screenResolution: '1900x1080',
+          visitorName: this.caps[Capabilities.EINSTEINBOT_VISITORNAME],
+          prechatDetails: [],
+          prechatEntities: [],
+          receiveQueueUpdates: true,
+          isPost: true
+        },
+        [CoreCapabilities.SIMPLEREST_PING_BODY_RAW]: true,
+        [CoreCapabilities.SIMPLEREST_PING_UPDATE_CONTEXT]: false,
+        [CoreCapabilities.SIMPLEREST_URL]: `${baseUrl}Chasitor/ChatMessage`,
+        [CoreCapabilities.SIMPLEREST_VERB]: 'POST',
+        [CoreCapabilities.SIMPLEREST_HEADERS_TEMPLATE]: {
+          'X-LIVEAGENT-API-VERSION': 34,
+          'X-LIVEAGENT-AFFINITY': '{{context.affinityToken}}',
+          'X-LIVEAGENT-SESSION-KEY': '{{context.key}}'
+        },
+        [CoreCapabilities.SIMPLEREST_BODY_TEMPLATE]: {
+          text: '{{msg.messageText}}'
+        },
+        [CoreCapabilities.SIMPLEREST_REQUEST_HOOK]: ({ requestOptions }) => {
+          requestOptions.headers['X-LIVEAGENT-SEQUENCE'] = this.seqCounter
+        },
+        [CoreCapabilities.SIMPLEREST_POLL_URL]: `${baseUrl}System/Messages`,
+        [CoreCapabilities.SIMPLEREST_POLL_VERB]: 'GET',
+        [CoreCapabilities.SIMPLEREST_POLL_HEADERS]: {
+          'X-LIVEAGENT-API-VERSION': 34,
+          'X-LIVEAGENT-AFFINITY': '{{context.affinityToken}}',
+          'X-LIVEAGENT-SESSION-KEY': '{{context.key}}'
+        },
+        [CoreCapabilities.SIMPLEREST_POLL_TIMEOUT]: 60000,
+        [CoreCapabilities.SIMPLEREST_POLL_UPDATE_CONTEXT]: false,
+        [CoreCapabilities.SIMPLEREST_BODY_JSONPATH]: '$.messages[?(@.type==\'ChatMessage\' || @.type==\'RichMessage\')].message',
+        [CoreCapabilities.SIMPLEREST_CONTEXT_MERGE_OR_REPLACE]: 'MERGE'
+      }
+      this.delegateCaps[CoreCapabilities.SIMPLEREST_RESPONSE_HOOK] = ({ botMsg, botMsgRoot }) => {
+        botMsg.messageText = botMsgRoot.text
+        if (botMsgRoot.items) {
+          botMsg.buttons = botMsgRoot.items.map(i => ({
+            text: i.text
+          }))
+        }
+      }
+
+      this.delegateCaps = Object.assign({}, this.caps, this.delegateCaps)
+
+      debug(`Validate delegateCaps ${util.inspect(this.delegateCaps)}`)
+      this.delegateContainer = new SimpleRestContainer({ queueBotSays: this.queueBotSays, caps: this.delegateCaps })
+    }
+
+    debug('Validate delegate')
+    return this.delegateContainer.Validate()
+  }
+
+  async Build () {
+    await this.delegateContainer.Build()
+  }
+
+  async Start () {
+    this.seqCounter = 1
+    await this.delegateContainer.Start()
+  }
+
+  async UserSays (msg) {
+    this.seqCounter = this.seqCounter + 1
+    await this.delegateContainer.UserSays(msg)
+  }
+
+  async Stop () {
+    await this.delegateContainer.Stop()
+  }
+
+  async Clean () {
+    await this.delegateContainer.Clean()
+  }
+}
+
+module.exports = {
+  PluginVersion: 1,
+  PluginClass: BotiumConnectorEinsteinBot,
+  PluginDesc: {
+    name: 'Einstein Bots',
+    provider: 'Salesforce',
+    avatar: fs.readFileSync(path.join(__dirname, 'logo.png')).toString('base64'),
+    capabilities: [
+      {
+        name: 'EINSTEINBOT_ENDPOINT',
+        label: 'Chat API Endpoint',
+        description: 'Find it in Salesforce: Service-Setup | Channels | Chat | Chat-Settings',
+        type: 'url',
+        required: true
+      },
+      {
+        name: 'EINSTEINBOT_ORGANIZATION_ID',
+        label: 'Organization Id',
+        description: 'Find it in Salesforce: Setup | Company Settings | Company information',
+        type: 'string',
+        required: true
+      },
+      {
+        name: 'EINSTEINBOT_DEPLOYMENT_ID',
+        label: 'Deployment Id',
+        description: 'Find it in Salesforce: Service-Setup | Channels | Chat | Deployments, click on deployment and get it from the page url (address=%2F<xxxxxx>)',
+        type: 'string',
+        required: true
+      },
+      {
+        name: 'EINSTEINBOT_CHATBUTTON_ID',
+        label: 'Chatbot Button Id',
+        description: 'Find it in Salesforce: Service-Setup | Channels | Chat | Chat Buttons, click on button and get it from the page url (address=%2F<xxxxxx>)',
+        description: 'A valid language isocode like en. If not provided a language detection will be performed.',
+        type: 'string',
+        required: true
+      },
+      {
+        name: 'EINSTEINBOT_VISITORNAME',
+        label: 'Visitor name',
+        description: `Visitor name to be used for the Botium conversations (default: "${Defaults[Capabilities.EINSTEINBOT_VISITORNAME]}")`,
+        type: 'string',
+        required: false
+      }
+    ]
+  }
+}
